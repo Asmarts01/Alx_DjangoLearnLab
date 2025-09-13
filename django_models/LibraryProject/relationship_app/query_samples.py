93626b4684ee392623e9635fa0bce24922944625
Aigbogun Anointed <aigbogunanointed71@gmail.com>
2025-09-13 18:53:21 +0100

    Add initial setup for LibraryProject with relationship app
    - Created manage.py for project management.
    - Added relationship_app with initial models: Author, Book, Library, and Librarian.
    - Implemented migrations for the models.
    - Set up admin configuration for the relationship app.
    - Created query_samples.py for querying books by author and library.
    - Added basic test and view files for future development.

---
+++ django_models/LibraryProject/relationship_app/query_samples.py
@@ -0,0 +1,34 @@
+import os
+import django
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_models.settings')
+django.setup()
+
+from relationship_app.models import Author, Book, Library, Librarian
+
+def books_by_author(author_name):
+    try:
+        author = Author.objects.get(name=author_name)
+        books= author.books.all()
+        return books
+    except Author.DoesNotExist:
+        return []
+    
+def books_in_library(library_name):
+    try:
+        library = Library.objects.get(name=library_name)
+        books = Library.book.through.objects.filter(library=library)
+        return [entry.book for entry in books]
+    except Library.DoedNotExist:
+        return []
+    
+def Librarian_of_library(library_name):
+    try:
+        Library = Library.objects.get(name=library_name)
+        return Library.librarian
+    except Library.DoesNotExist:
+        return[]
+    
+if __name__ == "__main__":
+    author_name = "J.K. Rowling"
+    print(f"Books by {author_name}")
