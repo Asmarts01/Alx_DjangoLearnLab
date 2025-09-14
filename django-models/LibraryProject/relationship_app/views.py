6e5a28664d7b9f3c0729f1802d7817a7b36177ee
Aigbogun Anointed <aigbogunanointed71@gmail.com>
2025-09-14 03:37:26 +0100

    Refactor list_books view to improve clarity and structure

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
 
-# Create your views here.
+# Create a function based view that list all books stored in the database
+# This view should render a simple text list of book titles and their authors.
+
+def list_books(request):
+    from relationship_app.models import Book
+    books = Book.objects.all()
+    book_list = "\n".join([f"{book.title} by {book.author.name}" for book in books])
+    return render(request, 'list_books.html', {'book_list': book_list})
+
+from django.http import HttpResponse
+def list_books_text(request):
+    from relationship_app.models import Book
+    books = Book.objects.all()
+    book_list = "\n".join([f"{book.title} by {book.author.name}" for book in books])
+    return HttpResponse(book_list, content_type="text/plain")
+
+
+
