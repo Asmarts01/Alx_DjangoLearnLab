6158f5e43e75236a3af9b89d6a1543ba866808bd
Aigbogun Anointed <aigbogunanointed71@gmail.com>
2025-09-18 10:13:23 +0100

    views updated

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -105,14 +105,14 @@ def add_book(request):
     return render(request, 'relationship_app/add_book.html')
 
 @permission_required('relationship_app.can_change_book')
-def change_book(request, book_id):
+def edit_book(request, book_id):
     book = get_object_or_404(Book, id=book_id)
     if request.method == 'POST':
         book.title = request.POST.get('title')
         book.author = request.POST.get('author')
         book.save()
         return redirect('list_books')
-    return render(request, 'relationship_app/change_book.html', {'book': book})
+    return render(request, 'relationship_app/edit_book.html', {'book': book})
 
 @permission_required('relationship_app.can_delete_book')
 def delete_book(request, book_id):
