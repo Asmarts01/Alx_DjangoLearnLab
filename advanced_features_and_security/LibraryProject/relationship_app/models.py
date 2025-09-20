a708e64650d8b5b168b0064a1579b0d224c17712
Aigbogun Anointed <aigbogunanointed71@gmail.com>
2025-09-21 00:05:57 +0100

    changes made to migrations

--- advanced_features_and_security/LibraryProject/relationship_app/models.py
+++ advanced_features_and_security/LibraryProject/relationship_app/models.py
@@ -1,17 +1,20 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.conf import settings
+# from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
 # Create your models here.
 class Author(models.Model):
     name = models.CharField(max_length=100)
+
     def __str__(self):
         return self.name
     
 class Book(models.Model):
     title = models.CharField(max_length=200)
     author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name='books')
+
     def __str__(self):
         return self.title
     
@@ -22,23 +25,31 @@ class Book(models.Model):
             ("can_change_book", "Can change book details"),
             ("can_delete_book", "Can delete a book"),
         ]
+
     def __str__(self):
         return self.title
     
 class Library(models.Model):
     name = models.CharField(max_length=100)
     book = models.ManyToManyField(Book)
+
     def __str__(self):
         return self.name
 
 class Librarian(models.Model):
     name = models.CharField(max_length=100)
     library = models.OneToOneField(Library, on_delete=models.CASCADE, related_name='librarian')
+
     def __str__(self):
         return self.name
     
 class UserProfile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    bio = models.TextField(blank=True, null=True)
+    profile_picture = models.ImageField(upload_to='profile_pics/', blank=True, null=True)
+
+    def __str__(self):
+        return self.user.username
 
 # role: CharField with choices for ‘Admin’, ‘Librarian’, and ‘Member’.
     role_choices = [
@@ -47,10 +58,11 @@ class UserProfile(models.Model):
         ('Member', 'Member'),
     ]
     role = models.CharField(max_length=20, choices=role_choices)
+
     def __str__(self):
         return f"{self.user.username} - {self.role}"
     
-@receiver(post_save, sender=User)
+@receiver(post_save, sender=settings.AUTH_USER_MODEL)
 def create_user_profile(sender, instance, created, **kwargs):
     if created:
         UserProfile.objects.create(user=instance)
