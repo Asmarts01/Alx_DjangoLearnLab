4b89b78d9647510cfe8e707ebcf9c7e7126a2f51
Aigbogun Anointed <aigbogunanointed71@gmail.com>
2025-09-21 01:09:51 +0100

    superuser created successfully

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -1,11 +1,30 @@
 from django.contrib import admin
-# Register your models here.
-from .models import Book
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
 
-class BookAdmin(admin.ModelAdmin):
 
-    list_display = ('title', 'author', 'publication_year')
-    list_filter = ('author', 'publication_year')
-    search_fields = ('title', 'author')
+class CustomUserAdmin(UserAdmin):
+    # Fields to display in the admin user list
+    list_display = ("username", "email", "date_of_birth", "is_staff", "is_superuser")
+    search_fields = ("username", "email")
+    ordering = ("username",)
 
-admin.site.register(Book, BookAdmin)
+    # Fields for the add/change forms
+    fieldsets = (
+        (None, {"fields": ("username", "email", "password")}),
+        ("Personal Info", {"fields": ("first_name", "last_name", "date_of_birth", "profile_photo")}),
+        ("Permissions", {"fields": ("is_active", "is_staff", "is_superuser", "groups", "user_permissions")}),
+        ("Important dates", {"fields": ("last_login", "date_joined")}),
+    )
+
+    # Fields used when creating a new user from admin
+    add_fieldsets = (
+        (None, {
+            "classes": ("wide",),
+            "fields": ("username", "email", "password1", "password2", "date_of_birth", "profile_photo"),
+        }),
+    )
+
+
+# Register your custom user model with the custom admin
+admin.site.register(CustomUser, CustomUserAdmin)
