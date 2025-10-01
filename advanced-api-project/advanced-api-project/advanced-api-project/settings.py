1fdac95d78c2662af6a9cf6590b7e478f81bd828
Aigbogun Anointed <aigbogunanointed71@gmail.com>
2025-10-01 15:10:36 +0100

    Fix settings.py formatting and update ROOT_URLCONF and WSGI_APPLICATION paths

--- advanced-api-project/advanced-api-project/advanced-api-project/settings.py
+++ advanced-api-project/advanced-api-project/advanced-api-project/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
@@ -49,7 +50,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'advanced_api_project.urls'
+ROOT_URLCONF = 'advanced-api-project.urls'
 
 TEMPLATES = [
     {
@@ -66,7 +67,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'advanced_api_project.wsgi.application'
+WSGI_APPLICATION = 'advanced-api-project.wsgi.application'
 
 
 # Database
